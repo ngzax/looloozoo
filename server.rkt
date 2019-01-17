576873fc2167ceb77c95305a9b7fcc015d9ab247
Daryl Richter <daryl.richter@nrg.com>
2019-01-17 18:36:03 -0500

    Another try
    What's going on with this info.rkt?

--- server.rkt
+++ server.rkt
@@ -15,7 +15,7 @@
                  8080))
 
 (serve/servlet start
-               #:servlet-path  "/"
+               #:servlet-path  ·"/"
                #:listen-ip     #f
                #:port          port
                #:command-line? #t)
